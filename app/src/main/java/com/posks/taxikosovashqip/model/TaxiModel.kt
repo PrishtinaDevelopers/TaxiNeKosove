87f3e0e99a79b8a6ccfa32571a68eea1db3ebf13
Drilon Reçica <drilonreqica@gmail.com>
2018-03-21 22:25:50 +0100

    Updated TaxiModel

--- app/src/main/java/com/posks/taxikosovashqip/model/TaxiModel.kt
+++ app/src/main/java/com/posks/taxikosovashqip/model/TaxiModel.kt
@@ -1,5 +1,4 @@
 package com.posks.taxikosovashqip.model
 
 class TaxiModel(var id: Long, var taxiName: String?, var citySlug: String?, var cityName: String?,
-                var isHasVala: Boolean, var numberVala: String?, var isHasIpko: Boolean,
-                var numberIpko: String?, var viberOption: Int)
+                var numberVala: String?, var numberIpko: String?, var viberOption: Int)
